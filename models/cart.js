f0392cfa2107bbc51b303df496bf762f20d53405
Maximiliano Frega <maxifrega21@gmail.com>
2020-11-06 14:41:58 -0300

    cambios en modelo

--- models/cart.js
+++ models/cart.js
@@ -10,10 +10,31 @@ const cart = new mongoose.Schema({
         type: Date,
         default: Date.now(),
     },
-    carrito: {
-        type: Object,
+    carrito: [{
+        articulo: {
+            type: String,
+            ref: 'Product',
+        },
+        cantidad: {
+            type: Number,
+            required: [true, 'La cantidad mínima debe ser 1(un) producto.'],
+        },
+        precio: Number,
+        total: Number,
+    }],
+    precioTotal: {
+        type: Number,
+        required: true,
     }
 })
+
+cart.pre(/^find/, function(next) {
+    this.populate('user').populate({
+        path: 'user',
+        select: 'nombre',
+    });
+    next();
+});
 
 const Cart = mongoose.model('Cart', cart);
 
